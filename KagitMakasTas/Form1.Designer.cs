2464f8f3b379aea61ee807a28d3eb43fa07ba1bc
eyyubtezcan <eyyub.tezcan@gmail.com>
2019-02-09 02:22:20 +0300

    update

---
+++ KagitMakasTas/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace KagitMakasTas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Taş";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(208, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 47);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Kağıt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(329, 274);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 47);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Makas";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(87, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(357, 89);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(194, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kağıt Makas Taş";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(87, 349);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Pc Score :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(87, 393);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "User Score :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(254, 349);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 31);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(254, 393);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 31);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ KagitMakasTas/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KagitMakasTas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int randomsayi;
+        int userscore = 0;
+        int pcscore = 0;
+        string[] sonuc;
+        Random rnd = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            randomsayi = rnd.Next(0, 3);
+            sonuc = new string[] { "Taş", "Kağıt", "Makas" };
+            label5.Text = pcscore.ToString();
+            label6.Text = userscore.ToString();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = sonuc[randomsayi];
+            if(randomsayi==0)
+            {
+                MessageBox.Show("Kazandınız");
+                userscore++;
+            }
+            else
+            {
+                MessageBox.Show("Kaybettiniz");
+                pcscore++;
+            }
+            randomsayi = rnd.Next(0, 3);
+            label5.Text = pcscore.ToString();
+            label6.Text = userscore.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = sonuc[randomsayi];
+            if (randomsayi == 1)
+            {
+                MessageBox.Show("Kazandınız");
+                userscore++;
+            }
+            else
+            {
+                MessageBox.Show("Kaybettiniz");
+                pcscore++;
+            }
+            randomsayi = rnd.Next(0, 3);
+            label5.Text = pcscore.ToString();
+            label6.Text = userscore.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = sonuc[randomsayi];
+            if (randomsayi == 2)
+            {
+                MessageBox.Show("Kazandınız");
+                userscore++;
+            }
+            else
+            {
+                MessageBox.Show("Kaybettiniz");
+                pcscore++;
+            }
+            randomsayi = rnd.Next(0, 3);
+            label5.Text = pcscore.ToString();
+            label6.Text = userscore.ToString();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.K)
+            {
+                label1.Text = sonuc[randomsayi];
+                if (randomsayi == 0)
+                {
+                    MessageBox.Show("Kazandınız");
+                    userscore++;
+                }
+                else
+                {
+                    MessageBox.Show("Kaybettiniz");
+                    pcscore++;
+                }
+                randomsayi = rnd.Next(0, 3);
+                label5.Text = pcscore.ToString();
+                label6.Text = userscore.ToString();
+            }
+            else if (e.KeyCode == Keys.M)
+            {
+                label1.Text = sonuc[randomsayi];
+                if (randomsayi == 1)
+                {
+                    MessageBox.Show("Kazandınız");
+                    userscore++;
+                }
+                else
+                {
+                    MessageBox.Show("Kaybettiniz");
+                    pcscore++;
+                }
+                randomsayi = rnd.Next(0, 3);
+                label5.Text = pcscore.ToString();
+                label6.Text = userscore.ToString();
+            }
+            else if (e.KeyCode == Keys.T)
+            {
+                label1.Text = sonuc[randomsayi];
+                if (randomsayi == 2)
+                {
+                    MessageBox.Show("Kazandınız");
+                    userscore++;
+                }
+                else
+                {
+                    MessageBox.Show("Kaybettiniz");
+                    pcscore++;
+                }
+                randomsayi = rnd.Next(0, 3);
+                label5.Text = pcscore.ToString();
+                label6.Text = userscore.ToString();
+            }
+        }
+
+        
+    }
+}
